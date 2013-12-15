57e65e6267e0091963043bb6ca7b871709e14dd2
adocilesloth <adocilesloth@gmail.com>
2013-12-15 16:47:25 +0000

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TS3Plugin.rc
+//
+#define IDD_TS3DLG                      101
+#define IPEdt                           1002
+#define UIDEdt                          1003
+#define PRFEdt                          1004
+#define PREFEdt                         1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
