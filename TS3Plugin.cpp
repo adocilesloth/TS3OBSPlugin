840bb84cf15396e62bfa03258702f1f109a75a91
adocilesloth <adocilesloth@gmail.com>
2013-12-20 12:55:03 +0000

    Added mute/deafen and change channel

--- TS3Plugin.cpp
+++ TS3Plugin.cpp
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 #include "resource.h"
 
 #include <fstream>
-#include <string>
 #include <sstream>
 
 #define WIN32_MEAN_AND_LEAN
@@ -30,20 +29,21 @@ using namespace std;
 int countSubstring(const string&, const string&);
 
 SOCKET obs;
-ofstream file;
+//ofstream file;
 ifstream settings;
 
 //config popout stuff
 HINSTANCE   hInstance;
-bool bPSVEnabled = FALSE;
+//cid for config
+string cid;
 
 INT_PTR CALLBACK ConfigDlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	LPWSTR ip = new wchar_t;	//recieve string
-	LPWSTR uid = new wchar_t;	//recieve string
-	LPWSTR pref = new wchar_t;	//recieve string
-	string sip, suid, spref;	//temp strings
-	wstring wip, wuid, wpref;	//sending strings
+	string sip, suid, spref, smute, schan, spw;		//temp strings
+
+	wstring wip, wuid, wpref, wcid, wpw;	//sending strings
+
+	bool bmute, bdeaf, bchan;		//bools for checkboxes
 	int length;
 	string path = OBSGetPluginDataPath().CreateUTF8String();
 	wofstream osettings;
@@ -65,27 +65,79 @@ INT_PTR CALLBACK ConfigDlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 	}
 	wpref = wstring(spref.begin(), spref.end());
 
+	getline(settings, smute);
+	if(smute == "1")
+	{
+		bmute = true;
+		bdeaf = false;
+	}
+	else if(smute == "2")
+	{
+		bmute = false;
+		bdeaf = true;
+	}
+	else if(smute == "3")
+	{
+		bmute = true;
+		bdeaf = true;
+	}
+	else
+	{
+		bmute = false;
+		bdeaf = false;
+	}
+
+	getline(settings, schan);
+	if(schan == "1")
+	{
+		bchan = true;
+	}
+	else
+	{
+		bchan = false;
+	}
+
+	getline(settings, spw);		//gets cid=..
+	spw.clear();				//so delete it
+	getline(settings, spw);		//gets cpw
+	spw = spw.substr(5, spw.length() - 5);	//remove  cpw=
+	wpw = wstring(spw.begin(), spw.end());
+
 	settings.close();
 
 	switch (message)
 	{
 	case WM_INITDIALOG:
 	{
-		if (wip.length() != 0)
+		if(wip.length() != 0)
 		{
 			HWND IPInput = GetDlgItem(hWnd, IPEdt);
 			SetWindowText(IPInput, wip.c_str());
 		}
-		if (wuid.length() != 0)
+		if(wuid.length() != 0)
 		{
 			HWND UIDInput = GetDlgItem(hWnd, UIDEdt);
 			SetWindowText(UIDInput, wuid.c_str());
 		}
 		HWND PREFInput = GetDlgItem(hWnd, PREFEdt);
 		SendMessage(PREFInput, EM_LIMITTEXT, 10, 0);	//limit prefix to 10 chars
-		if (wpref.length() != 0)
+		if(wpref.length() != 0)
 		{
 			SetWindowText(PREFInput, wpref.c_str());
+		}
+		SendMessage(GetDlgItem(hWnd, MUTChk), BM_SETCHECK, bmute ? BST_CHECKED : BST_UNCHECKED, 0);
+		SendMessage(GetDlgItem(hWnd, DEFChk), BM_SETCHECK, bdeaf ? BST_CHECKED : BST_UNCHECKED, 0);
+		SendMessage(GetDlgItem(hWnd, CHANChk), BM_SETCHECK, bchan ? BST_CHECKED : BST_UNCHECKED, 0);
+		if(bchan)
+		{
+			EnableWindow(GetDlgItem(hWnd, CHANBut), true);
+			ShowWindow(GetDlgItem(hWnd, CHANTxt), SW_SHOW);
+			EnableWindow(GetDlgItem(hWnd, PWEdt), true);
+		}
+		if(wpw.length() != 0)
+		{
+			HWND PWInput = GetDlgItem(hWnd, PWEdt);
+			SetWindowText(PWInput, wpw.c_str());
 		}
 	}
 
@@ -117,9 +169,49 @@ INT_PTR CALLBACK ConfigDlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 			length = GetWindowTextLength(PREFOutput) + 1;
 			TCHAR * prefbuf = (TCHAR *)GlobalAlloc(GPTR, length * sizeof(TCHAR));
 			GetWindowText(PREFOutput, prefbuf, length);
-			osettings << prefbuf;		//write to file
-			GlobalFree(prefbuf);		//clear buffer
-			
+			osettings << prefbuf << endl;	//write to file
+			GlobalFree(prefbuf);			//clear buffer
+			//mute and deafened
+			bmute = SendMessage(GetDlgItem(hWnd, MUTChk), BM_GETCHECK, 0, 0) == BST_CHECKED;
+			bdeaf = SendMessage(GetDlgItem(hWnd, DEFChk), BM_GETCHECK, 0, 0) == BST_CHECKED;
+			if(!bmute && !bdeaf)
+			{
+				osettings << "0" << endl;
+			}
+			else if(bmute && !bdeaf)
+			{
+				osettings << "1" << endl;
+			}
+			else if(!bmute && bdeaf)
+			{
+				osettings << "2" << endl;
+			}
+			else if(bmute && bdeaf)
+			{
+				osettings << "3" << endl;
+			}
+			//channel switch
+			bchan = SendMessage(GetDlgItem(hWnd, CHANChk), BM_GETCHECK, 0, 0) == BST_CHECKED;
+			if(bchan)
+			{
+				osettings << "1" << endl;
+			}
+			else
+			{
+				osettings << "0" << endl;
+			}
+			//cid
+			wcid = wstring(cid.begin(), cid.end());
+			osettings << wcid << endl;
+			//password
+			HWND PWOutput = GetDlgItem(hWnd, PWEdt);
+			length = GetWindowTextLength(PWOutput) + 1;
+			TCHAR * pwbuf = (TCHAR *)GlobalAlloc(GPTR, length * sizeof(TCHAR));
+			GetWindowText(PWOutput, pwbuf, length);
+			osettings << " cpw="
+					  << pwbuf;			//write to file
+			GlobalFree(pwbuf);			//clear buffer
+
 			//clean up
 			osettings.close();
 			
@@ -131,6 +223,73 @@ INT_PTR CALLBACK ConfigDlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 		case IDCANCEL:
 			EndDialog(hWnd, LOWORD(wParam));
 			break;
+
+		case CHANChk:
+		{
+			bchan = SendMessage(GetDlgItem(hWnd, CHANChk), BM_GETCHECK, 0, 0) == BST_CHECKED;
+			if(bchan)
+			{
+				EnableWindow(GetDlgItem(hWnd, CHANBut), true);
+				ShowWindow(GetDlgItem(hWnd, CHANTxt), SW_SHOW);
+				EnableWindow(GetDlgItem(hWnd, PWEdt), true);
+			}
+			else if(!bchan)
+			{
+				EnableWindow(GetDlgItem(hWnd, CHANBut), false);
+				ShowWindow(GetDlgItem(hWnd, CHANTxt), SW_HIDE);
+				EnableWindow(GetDlgItem(hWnd, PWEdt), false);
+			}
+		}
+		case CHANBut:
+		{
+			char *adrs = getIP();
+
+			if(!ConnectToHost(25639, adrs))
+			{
+				break;
+			}
+
+			int iResult;
+			int i = 0;
+			char *whoami = "whoami\n";
+			char reci[256];
+			string truereci;
+
+			do
+			{
+				iResult = send(obs, whoami, (int)strlen(whoami), 0);	//request whoami
+				if (iResult == SOCKET_ERROR)
+				{
+					AppWarning(TEXT("whoami Send Failure"));
+					break;
+				}
+
+				iResult = recv(obs, reci, 256, 0);	//recieve result: clid=XX cid=XXXX
+				if (iResult == SOCKET_ERROR)
+				{
+					AppWarning(TEXT("whoami Recieve Failure"));
+					break;
+				}
+
+				truereci = reci;
+				truereci = truereci.substr(0, 5);
+				i++;
+			}while (truereci != "clid=" && i < 10);	//while reci returns the wrong string
+			
+			if(truereci != "clid=")
+			{
+				break;
+			}
+
+			cid = reci;
+			size_t startpos = cid.find("cid=");
+			size_t endpos = cid.find("\n");
+			cid = cid.substr(startpos, endpos - startpos);
+
+			CloseConnection();
+			break;
+		}
+
 		}
 	}
 
@@ -152,11 +311,21 @@ bool LoadPlugin()
 		ofstream create(path + "\\ts3.ini");
 		create << "127.0.0.1" << endl;
 		create << "cluid=" << endl;
-		create << "*R*";
-		create.close();			//stop using settings file
+		create << "*R*" << endl;
+		create << "0" << endl;
+		create << "0" << endl;
+		create << "cid=1" << endl;
+		create << " cpw=";
+		create.close();		//stop using settings file
+
+		cid = "cid=1";		//set cid string
 	}
 	else
 	{
+		for(int i = 0; i < 6; i++)
+		{
+			getline(settings, cid);	//set cid string
+		}
 		settings.close();		//stop using settings file
 	}
 
@@ -166,7 +335,16 @@ bool LoadPlugin()
 
 void UnloadPlugin()
 {
-
+	//get settings file path
+	string path = OBSGetPluginDataPath().CreateUTF8String();
+	path.append("/ts3temp.ini");
+	//test if ts3temp.ini exists. If yes, delete
+	settings.open(path);
+	if(settings.is_open())
+	{
+		settings.close();
+		remove(path.c_str());
+	}
 }
 
 CTSTR GetPluginName()
@@ -176,21 +354,21 @@ CTSTR GetPluginName()
 
 CTSTR GetPluginDescription()
 {
-	return TEXT("Adds *R* (or any other prefix) before TS3 nickname when recording. Removes the prefix when not recording.");
+	return TEXT("Adds *R* (or any other prefix) before TS3 nickname when recording. Options to mute/deafen self and move channel while recording. Undos all effects when recording stops.");
 }
 
 void OnStartStream()
 {
 	char *adrs = getIP();
+	bool start;
 
 	if(!ConnectToHost(25639, adrs))
 	{
 		return;
 	}
-	if (Communicate(1))
-	{
-		return;
-	}
+	start = Communicate(1);
+	start = MuteandDeafen(1);
+	start = ChannelSwitch(1);
 	CloseConnection();
 
 	return;
@@ -199,16 +377,15 @@ void OnStartStream()
 void OnStopStream()
 {
 	char *adrs = getIP();
+	bool stop;
 
 	if(!ConnectToHost(25639, adrs))
 	{
 		return;
 	}
-	if(!Communicate(0))
-	{
-		CloseConnection();
-		return;
-	}
+	stop = Communicate(0);
+	stop = MuteandDeafen(0);
+	stop = ChannelSwitch(0);
 	CloseConnection();
 
 	return;
@@ -426,6 +603,259 @@ bool Communicate(int cont)
 	}
 
 	//file.close();
+	settings.close();
+	return true;
+}
+
+bool MuteandDeafen(int state)
+{
+	//get settings file path
+	string path = OBSGetPluginDataPath().CreateUTF8String();
+	//get mute and deafen settings from ts3.ini
+	settings.open(path + "\\ts3.ini");
+	string mnd;
+	getline(settings, mnd);		//ip
+	mnd.clear();
+	getline(settings, mnd);		//cluid
+	mnd.clear();
+	getline(settings, mnd);		//prefix
+	mnd.clear();
+	getline(settings, mnd);		//mute and deafened state
+
+	if(mnd != "1" && mnd != "2" && mnd != "3")	//if not set to mute or deafen
+	{
+		settings.close();
+		return true;
+	}
+
+	int iResult;
+	stringstream sstate;
+	sstate << state << "\n";
+
+	if(mnd == "1" || mnd == "3")	//if set to mute
+	{
+		char reci1[256];
+		int i = 0;
+		string truereci1;
+
+		string tempmute = "clientupdate client_input_muted=";
+		tempmute.append(sstate.str());
+		const char *mute = tempmute.c_str();
+
+		do
+		{
+			iResult = send(obs, mute, (int)strlen(mute), 0);	//set mute
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Mute Send Failure"));
+				settings.close();
+				return false;
+			}
+
+			iResult = recv(obs, reci1, 256, 0);	//recieve result: error id=0...
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Mute Recieve Failure"));
+				settings.close();
+				return false;
+			}
+
+			truereci1 = reci1;
+			truereci1 = truereci1.substr(0, 10);
+			i++;
+		}while (truereci1 != "error id=0" && i < 10);
+	}
+
+	if(mnd == "2" || mnd == "3")
+	{
+		char reci2[256];
+		int j = 0;
+		string truereci2;
+	
+		string tempdeaf = "clientupdate client_output_muted=";
+		tempdeaf.append(sstate.str());
+		const char *deaf = tempdeaf.c_str();
+
+		do
+		{
+			iResult = send(obs, deaf, (int)strlen(deaf), 0);	//set deafen
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Deaf Send Failure"));
+				settings.close();
+				return false;
+			}
+
+			iResult = recv(obs, reci2, 256, 0);	//recieve result: error id=0...
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Deaf Recieve Failure"));
+				settings.close();
+				return false;
+			}
+
+			truereci2 = reci2;
+			truereci2 = truereci2.substr(0, 10);
+			j++;
+		}while (truereci2 != "error id=0" && j < 10);
+	}
+	
+	settings.close();
+	return true;
+}
+
+bool ChannelSwitch(int state)
+{
+	//get settings file path
+	string path = OBSGetPluginDataPath().CreateUTF8String();
+	//get mute and deafen settings from ts3.ini
+	settings.open(path + "\\ts3.ini");
+	string swtch;
+	getline(settings, swtch);		//ip
+	swtch.clear();
+	getline(settings, swtch);		//cluid
+	swtch.clear();
+	getline(settings, swtch);		//prefix
+	swtch.clear();
+	getline(settings, swtch);		//mute and deafened state
+	swtch.clear();
+	getline(settings, swtch);		//switch state
+
+	if(swtch != "1")	//if not set to mute or deafen
+	{
+		settings.close();
+		return true;
+	}
+
+	int iResult;
+	int i = 0;
+	char *whoami = "whoami\n";
+	char reci[256];
+	string truereci, tcid, rcid, clid, cpw;
+
+	do
+	{
+		iResult = send(obs, whoami, (int)strlen(whoami), 0);	//request whoami
+		if (iResult == SOCKET_ERROR)
+		{
+			AppWarning(TEXT("whoami Send Failure"));
+			settings.close();
+			return false;
+		}
+
+		iResult = recv(obs, reci, 256, 0);	//recieve result: clid=XX cid=XXXX
+		if (iResult == SOCKET_ERROR)
+		{
+			AppWarning(TEXT("whoami Recieve Failure"));
+			settings.close();
+			return false;
+		}
+
+		truereci = reci;
+		truereci = truereci.substr(0, 5);
+		i++;
+	}while (truereci != "clid=" && i < 10);	//while reci returns the wrong string
+
+	clid = reci;
+	size_t space = clid.find("cid=");
+	clid = clid.substr(0, space);
+
+	string channel = "clientmove ";
+	channel.append(clid);
+
+	int j = 0;
+	char reci2[256];
+	string truereci2;
+
+	if(state == 1)	//switch to channel
+	{
+		//write return cid to file
+		rcid = reci;
+		size_t startpos = rcid.find("cid=");
+		size_t endpos = rcid.find("\n");
+		rcid = rcid.substr(startpos, endpos - startpos);
+
+		ofstream rturn(path + "\\ts3temp.ini");
+		rturn << rcid;
+		rturn.close();
+
+		//move channel
+		getline(settings, tcid);
+		getline(settings, cpw);
+		channel.append(tcid);
+		channel.append(cpw);
+		channel.append("\n");
+		const char *move = channel.c_str();
+
+		do
+		{
+			iResult = send(obs, move, (int)strlen(move), 0);	//set deafen
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Move Send Failure"));
+				settings.close();
+				return false;
+			}
+
+			iResult = recv(obs, reci2, 256, 0);	//recieve result: error id=0...
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Move Recieve Failure"));
+				settings.close();
+				return false;
+			}
+
+			truereci2 = reci2;
+			truereci2 = truereci2.substr(0, 10);
+			j++;
+		}while (truereci2 != "error id=0" && j < 10);
+	}
+
+	else	//switch back
+	{
+		//get return cid
+		ifstream rturn(path + "\\ts3temp.ini");
+		if(!rturn.is_open())	//set target as same channel
+		{
+			rcid = reci;
+			size_t startpos = rcid.find("cid=");
+			size_t endpos = rcid.find("\n");
+			rcid = rcid.substr(startpos, endpos - startpos);	
+		}
+		else
+		{
+			getline(rturn, rcid);
+		}
+		rturn.close();
+
+		channel.append(rcid);
+		channel.append("\n");
+		const char *move = channel.c_str();
+
+		do
+		{
+			iResult = send(obs, move, (int)strlen(move), 0);	//set deafen
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Move Send Failure"));
+				settings.close();
+				return false;
+			}
+
+			iResult = recv(obs, reci2, 256, 0);	//recieve result: error id=0...
+			if (iResult == SOCKET_ERROR)
+			{
+				AppWarning(TEXT("Move Recieve Failure"));
+				settings.close();
+				return false;
+			}
+
+			truereci2 = reci2;
+			truereci2 = truereci2.substr(0, 10);
+			j++;
+		}while (truereci2 != "error id=0" && j < 10);
+	}
+
 	settings.close();
 	return true;
 }
